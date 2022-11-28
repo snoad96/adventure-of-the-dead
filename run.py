0c8970d9834d0be87c04d7bab34e7984d3e0b6df
DSnoad <danialsnoad@hotmail.com>
2022-11-28 15:51:05 +0000

    Create baseline of the beginning of game

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def intro():
+    """ 
+    This will introduce the game to the player.
+    """
+    print(
+        "Welcome to the Adventure of the Dead, A story based walkthrough game" 
+        "Inspired by the Classic Movie: Shaun of the Dead"
+    )
+
+
+def start_game():
+    """ 
+    This is the beginning of the Game, giving the player an option of 
+    how they would like to start.
+    """
+
+
+def play_again():
+    """ 
+    Option to play the game again.
+    """
