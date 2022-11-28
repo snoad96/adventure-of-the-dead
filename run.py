631ff787c77b55b1b90de4020ea4adea4ec4b5e1
DSnoad <danialsnoad@hotmail.com>
2022-11-28 17:15:58 +0000

    Define intro function

--- run.py
+++ run.py
@@ -2,10 +2,18 @@ def intro():
     """ 
     This will introduce the game to the player.
     """
-    print(
-        "Welcome to the Adventure of the Dead, A story based walkthrough game" 
-        "Inspired by the Classic Movie: Shaun of the Dead"
-    )
+    print("Welcome to the Adventure of the Dead, A story based walkthrough") 
+    print("Inspired by the Classic Movie: Shaun of the Dead")
+    print("Will you join us on this adventure? (yes/no)")
+    answer = input("").lower().strip()
+    while answer not in [YES, NO]:
+        print("That doesn't work... Try again")
+        answer = input("").lower().strip()
+    if answer in YES:
+        print("We may have to kill my Stepdad")
+
+
+intro()
 
 
 def start_game():
